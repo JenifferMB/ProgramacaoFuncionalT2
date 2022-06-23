06f2965d682b61f5889fd6f463697861f13c2bd3
Jeniffer Moreira Borges <jenifferrborges@gmail.com>
2022-06-23 17:26:14 -0300

    logica 2

--- trab2.hs
+++ trab2.hs
@@ -1,3 +1,5 @@
+import Data.Char (digitToInt)
+
 type CIN = String
 
 addSum :: CIN -> CIN
@@ -6,5 +8,11 @@ addSum cin = cin ++ show (sum (map (\x -> read [x]::Int) cin))
 validar :: CIN -> Bool
 validar x = validar1 x == validar2 x
     where
-        validar1 x = sum (take 8 x)
-        validar2 x = (take 2(reverse x))
+        validar1 x = citIntSum (take 8 x) -- seria o equivalente a somar os 8 primeiros digitos da string, citIntSum soma, falta encaixar o take 8
+        validar2 x = (take 2(reverse x)) -- seria o equivalente a reverter a string, e concatectar a primeira posição + a segunda posição
+
+citIntSum :: CIN -> Int
+citIntSum = foldr ((+) . digitToInt) 0
+ 
+
+-- método de validar = verificar se a soma dos 8 primeiros digitos (validar1) é igual a concatenação dos ultimos dois digitos (reverter usar o take 2 para pegar os dois digitos)
